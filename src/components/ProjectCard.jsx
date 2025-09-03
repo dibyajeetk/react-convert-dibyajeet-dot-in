db499d8cfb133a1fed581f1f9f43d73e81052a7f
dibyajeetk <dibyajeet.creates@gmail.com>
2025-09-03 14:23:56 +0530

    feat: card hover transition done

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,26 +1,36 @@
 import "../css/ProjectCard.css";
+import React, { useState } from "react";
+
 function ProjectCard() {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <div className="card-container">
-      <div className="default-state">
-        <img
-          className="cover-image"
-          src="./src/assets/test-cover.png"
-          alt="project title"
-        />
-      </div>
-      <div className="hover-state">
-        <h2 className="project-title">Admin Settings Redesign</h2>
-        <p className="project-description">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-          laboriosam a deleniti magni sunt nostrum.
-        </p>
-        <div className="tags-container">
-          <span className="tag">
-            <i className="bi bi-hash sm-icon"></i>
-            <p className="tag-text">ui design</p>
-          </span>
-        </div>
+    <div
+      className="card-container"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <div className={isHovered ? "hover-state" : "default-state"}>
+        {!isHovered ? (
+          <img
+            className="cover-image"
+            src="./src/assets/test-cover.png"
+            alt="project title"
+          />
+        ) : (
+          <>
+            <h2 className="project-title">Admin Settings Redesign</h2>
+            <p className="project-description">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
+              laboriosam a deleniti magni sunt nostrum.
+            </p>
+            <div className="tags-container">
+              <span className="tag">
+                <i className="bi bi-hash sm-icon"></i>
+                <p className="tag-text">ui design</p>
+              </span>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
