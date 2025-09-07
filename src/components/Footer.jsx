5d539fa8797516edf89cc1a20ffd6e96d4173523
dibyajeetk <dibyajeet.creates@gmail.com>
2025-09-07 13:36:16 +0530

    feat: footer design closed!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,37 @@ function Footer() {
       <h2 className="thanks-footer">Thanks for stopping by, let's chat! 🤙</h2>
       <div className="footer-details">
         <span className="footer-group">
-          <p className="footer-small-headers">Contact Me here</p>
-          <p className="footer-copy">ux.dibyajeet@gmail.com</p>
+          <p className="footer-small-headers">Contact Me</p>
+          <a href="mailto:ux.dibyajeet@gmail.com" className="footer-copy">
+            ux.dibyajeet@gmail.com
+          </a>
         </span>
         <span className="footer-group">
           <p className="footer-small-headers">Let's Connect</p>
-          <p className="footer-copy">ux.dibyajeet@gmail.com</p>
+          <span className="icon-holder">
+            <a
+              href="https://www.linkedin.com/in/dibyajeetk/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn profile"
+            >
+              <i className="bi bi-linkedin footer-icon"></i>
+            </a>
+            <a
+              href="https://www.instagram.com/97ronen/"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="LinkedIn profile"
+            >
+              <i className="bi bi-instagram footer-icon"></i>
+            </a>
+          </span>
         </span>
         <span className="footer-group">
           <p className="footer-small-headers">
             &copy; 2025 Dibyajeet Kirttania, all rights reserved.
           </p>
-          <p className="footer-copy">
-            <span className="emphasis">Last updated on:</span>02 September, 2025
-          </p>
+          <p className="footer-copy">Last updated on: 02 September, 2025</p>
         </span>
       </div>
     </footer>
